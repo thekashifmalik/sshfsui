9f55a1749b96c1aede4c009e81ea1e6a5309d44e
thekashifmalik <emailkashifmalik@gmail.com>
2024-07-04 14:02:03 -0700

    Sign code for MacOS

--- forge.config.cjs
+++ forge.config.cjs
@@ -4,7 +4,8 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-  },
+    osxSign: {},
+ },
   rebuildConfig: {},
   makers: [
     {
